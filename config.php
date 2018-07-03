b52b575570b93dff0c8ac18cf0a5eba91265d7f1
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-03 08:15:26 +0300

    test

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+
+define('ID', '6622364');
+define('SECRET', 'mhhtPWzYiWgpBsnThur0');
+define('URL', 'http://site-ng-vk-oauth.tk/vk.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<? include 'config.php' ?>
+
+<a href='https://oauth.vk.com/authorize?client_id=<?=ID?>&display=page&redirect_uri=<?=URL?>&scope=friends&response_type=token&v=5.52' target="_blank"> Войти через vk </a>
